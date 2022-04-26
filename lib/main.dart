b313e8694944289148716dea34296a74b78cc02f
LGranados <maui@MacBook-Air-de-LGranados.local>
2022-04-26 14:18:01 -0500

    FLUTTER-COUNTER-003 Eliminacion de WARNING

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+  class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Center(child: Text('¡Hola mundo!')),
+    return const MaterialApp(
+      home: Center(
+        child:  Text('¡Hola mundo!'),
+        )
     );
     // TODO: implement build
     throw UnimplementedError();
